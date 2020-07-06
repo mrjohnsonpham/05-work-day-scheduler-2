bc7868abf4cb41b521c81f77e5a53412dc4c703f
Johnson Pham <mrjohnsonpham@gmail.com>
2020-07-06 15:26:37 -0400

    Array of Numbers that represent the Hours

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ var timeDisplay = moment().format('MMMM Do YYYY, h:mm:ss a');
 var timeDiv = $("#currentDay");
 timeDiv.append(timeDisplay);
 
+// Array of Numbers that represent the Hours
+
+var timeList = [
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 AM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM"
+];
+
+var idTime = [
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17"
+];
